3040d03acd8ef32256c83b96d833311d3d02e028
Kostyantyn <kostyantyn@gmail.com>
2013-08-14 18:59:48 +0100

    ticket:1 garbage removed

---
+++ cupstest/contacts/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from contacts.models import Contact
+from django.utils import timezone
+
+class ContactModelTest(TestCase):
+    def test_creating_a_new_contact_and_save_it_to_db(self):
+        #create a new contact
+        contact = Contact()
+        contact.name = 'Kostyantyn'
+        contact.surname = 'Surename'
+        contact.birthdate = timezone.now()
+        contact.bio = 'My bio'
+        contact.contacts = 'contacts'
+
+        contact.Save()
+
+        all_contacts = Contact.objects.all()
+        self.assertEqual(len(all_contacts), 1)
+        db_contact = all_contacts[0]
+        self.assertEqual(db_contact, contact)
+
+        self.assertEqual(db_contact.name, 'Kostyantyn')
+        self.assertEqual(db_contact.surname, 'Surename')
+        self.assertEqual(db_contact.birthdate, contact.birthdate)
+        self.assertEqual(db_contact.bio, 'My bio')
+        self.assertEqual(db_contact.contacts, 'contacts')
--- cupstest/tests/tests.py
+++ cupstest/tests/tests.py
@@ -3,28 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class AdminTest(LiveServerTestCase):
-  def setUp(self):
-    self.browser = webdriver.Firefox()
-    self.browser.implicitly_wait(3)
+    fixtures=['admin_user.json']
 
-  def tearDown(self):
-    self.browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-  def test_can_login_to_admin_site(self):
-    self.browser.get(self.live_server_url+'/admin/')
+    def tearDown(self):
+        self.browser.quit()
 
-    body = self.browser.find_element_by_tag_name('body')
-    self.assertIn('Django administration', body.text)
-    
-    username_field = self.browser.find_element_by_name('username')
-    username_field.send_keys('admin')
-    
-    password_field = self.browser.find_element_by_name('password')
-    password_field.send_keys('admin')
-    password_field.send_keys(Keys.RETURN)
-    
-    body = self.browser.find_element_by_tag_name('body')
-    self.assertIn('Site administration', body.text)
-    
-    contact_links = self.browser.find_element_by_link_text('Contacts')
-    self.assertEqual(len(contact_links), 2)
+    def test_can_login_to_admin_site(self):
+        self.browser.get(self.live_server_url+'/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+        contact_links = self.browser.find_elements_by_link_text('Contacts')
+        self.assertEqual(len(contact_links), 2)
