cee2024f141b674dea6b4face855bc5f0e316da1
Kostyantyn <kostyantyn@gmail.com>
2013-08-15 13:43:59 +0100

    ticket:1 forgot contacts model

---
+++ contacts/migrations/0002_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Contact'
+        db.create_table(u'contacts_contact', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('surname', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('birthdate', self.gf('django.db.models.fields.DateTimeField')()),
+            ('bio', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('contacts', self.gf('django.db.models.fields.CharField')(max_length=256)),
+        ))
+        db.send_create_signal(u'contacts', ['Contact'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Contact'
+        db.delete_table(u'contacts_contact')
+
+
+    models = {
+        u'contacts.contact': {
+            'Meta': {'object_name': 'Contact'},
+            'bio': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'birthdate': ('django.db.models.fields.DateTimeField', [], {}),
+            'contacts': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['contacts']
