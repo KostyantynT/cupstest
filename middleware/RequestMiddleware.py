c7ef6fcd70cf4cf06009b42c83a529e0fb651e96
KostyantynT <kostyantyn@gmail.com>
2013-08-22 17:50:50 +0100

    ticket:3 fixes according to comments

--- middleware/RequestMiddleware.py
+++ middleware/RequestMiddleware.py
@@ -2,11 +2,5 @@ from models import RequestLog
 
 class RequestMiddleware(object):
     def process_request(self, request):
-        try:
-            requestlog = RequestLog()
-            requestlog.path = request.path;
-            requestlog.save();
-        except:
-            pass
-        
+        RequestLog.objects.create(path=request.path)
         return None
