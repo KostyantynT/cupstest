fd2659e0019a436c88aa7764e1a92db4fada7b75
KostyantynT <kostyantyn@gmail.com>
2013-08-23 14:11:37 +0100

    ticket:5 implemented according to reviewer comments

--- cupstest/context_processors.py
+++ cupstest/context_processors.py
@@ -1,7 +1,4 @@
 from django.conf import settings
 
 def django_settings(request):
-	result = {}
-	for s in dir(settings):
-		result[s] = getattr(settings, s)
-	return result
+	return {'settings':settings}
